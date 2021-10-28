62c876492afdfd31edeaab62dfc091b115724d8f
manish1993singh <manish1993singh@gmail.com>
2021-10-29 00:17:25 +0530

    testcases

--- test/basic.test.js
+++ test/basic.test.js
@@ -4,7 +4,7 @@
 
 import React from 'react';
 import PopupUtils from "../index";
-import {cleanup, fireEvent, render, screen} from '@testing-library/react';
+import {cleanup, fireEvent, render, screen, waitFor} from '@testing-library/react';
 
 afterAll(cleanup);
 
@@ -23,4 +23,34 @@ it('closing on outside click', ()=>{
     const {getByText, } = render(<div><div>beta</div><PopupUtils closeOnSelfClick={true} closePopup={()=>cleanup()}><div>alpha</div></PopupUtils></div>);
     fireEvent.click(getByText('beta'));
     expect(screen.queryByText('alpha')).not.toBeTruthy();
+})
+
+it('close after timeout', async ()=>{
+    render(<PopupUtils setTimeout={true} closePopup={()=>cleanup()}><div>alpha</div></PopupUtils>);
+    await new Promise(resolve => setTimeout(resolve,2000))
+    expect(screen.queryByText('alpha')).not.toBeTruthy()
+})
+
+it('close after 100ms timeout', async ()=>{
+    render(<PopupUtils timeoutTime={100} setTimeout={true} closePopup={()=>cleanup()}><div>alpha</div></PopupUtils>);
+    await new Promise(resolve => setTimeout(resolve,200));
+    expect(screen.queryByText('alpha')).not.toBeTruthy()
+})
+
+it('close on esc key', async ()=>{
+    render(<PopupUtils closePopup={()=>cleanup()}><div>alpha</div></PopupUtils>);
+    fireEvent.keyDown(screen.queryByText('alpha'),{key: "Escape",
+        code: "Escape",
+        keyCode: 27,
+        charCode: 27})
+    expect(screen.queryByText('alpha')).not.toBeTruthy()
+})
+
+it('not close on esc key', async ()=>{
+    render(<PopupUtils closeOnEscape={false} closePopup={()=>cleanup()}><div>alpha</div></PopupUtils>);
+    fireEvent.keyDown(screen.queryByText('alpha'),{key: "Escape",
+        code: "Escape",
+        keyCode: 27,
+        charCode: 27})
+    expect(screen.queryByText('alpha')).toBeTruthy()
 })
